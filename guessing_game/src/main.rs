54e1c735676b311d049f490f2cc36e27e5c0fdcd
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-13 12:32:49 -0500

    [update] added parsing to entered input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,11 +11,11 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();
-
+    
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line.");
-
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {}", guess);
     match guess.cmp(&secret_number) {
         Less => println!("Too small!"),
