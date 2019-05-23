21c506e61d923c4aed0e61cc69c9dd54babef15f
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-23 00:08:06 -0500

    [add] add ownership sample

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut first_word = String::new();
+    println!("Type an statement:");
+    io::stdin()
+        .read_line(&mut first_word)
+        .expect("Not a valid string!");
+    let first_word = get_first_word(&first_word);
+    println!("The first word is: {}", first_word);
+    
+}
+
+
+fn get_first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[0..]
 }
