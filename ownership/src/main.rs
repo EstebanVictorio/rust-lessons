fb4abb709b07b823ff927b9dcee715ed700ce0cf
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-24 16:31:55 -0500

    [update] added ownership sample function

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -9,7 +9,13 @@ fn main() {
         .expect("Not a valid string!");
     let first_word = get_first_word(&first_word);
     println!("The first word is: {}", first_word);
-    
+    let mut first_word_ownership = String::new();
+    println!("Type again:");
+    io::stdin()
+        .read_line(&mut first_word_ownership)
+        .expect("Not a valid string!");
+    let first_word_ownership = get_first_word_ownership(first_word_ownership);
+    println!("The first word is: {}", first_word_ownership);
 }
 
 
@@ -23,3 +29,15 @@ fn get_first_word(s: &str) -> &str {
 
     &s[0..]
 }
+
+fn get_first_word_ownership (s: String) -> String {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return s.chars().take(i).collect()
+        }
+    }
+     s
+}
+
+
