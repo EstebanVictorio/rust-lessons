b85f968ff288ed772faaa05151ef5a624754480d
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-26 11:25:54 -0500

    [update] add option example on enum sample

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -10,9 +10,26 @@ enum ECMA_ADV {
     ES6(String)
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
 fn main() {
     let standard = ECMA::ES5;
     let std_adv = ECMA_ADV::ES5(String::from("Using std ES5."));
+    let msg = Message::Quit;
+    let option_1: Option<u32> = Some(12);
+    let option_2 : Option<u32> = None;
+    msg.call();
+    println!("Option 1: {}", option_1.unwrap());
+    println!("Option 2: {}", option_2.unwrap()); // This panics, since we cannot unwrap a value from 'None'
     match standard {
         ECMA::ES5 => println!("You are using the ES5 standard"),
         ECMA::ES6 => println!("You are using the ES6 standard"),
