9d83f6fe0237005ac99b761539a1b55a3b61aac9
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-26 12:10:58 -0500

    [add] add if let sample

---
+++ if-let/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let purchase_result = false;
+    if let true = purchase_result {
+        println!("Purchased successfully!");
+    } else {
+        println!("Purchased failed.");
+    }
+}
