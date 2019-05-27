c7ab4b5de8cfd304cd2fcd9a0b0d8e6de757be1d
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-27 16:43:25 -0500

    [update] add module sample

---
+++ restaurant/target/debug/deps/restaurant-c8f2c4b792c592b1.d
@@ -0,0 +1,7 @@
+/Users/estebanburgueno/Projects/rust-lessons/restaurant/target/debug/deps/restaurant-c8f2c4b792c592b1.rmeta: src/lib.rs
+
+/Users/estebanburgueno/Projects/rust-lessons/restaurant/target/debug/deps/librestaurant-c8f2c4b792c592b1.rlib: src/lib.rs
+
+/Users/estebanburgueno/Projects/rust-lessons/restaurant/target/debug/deps/restaurant-c8f2c4b792c592b1.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/Users/estebanburgueno/Projects/rust-lessons/restaurant/target/debug/librestaurant.rlib: /Users/estebanburgueno/Projects/rust-lessons/restaurant/src/lib.rs
