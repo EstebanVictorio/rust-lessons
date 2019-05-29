57c1d9ea0bb2850eb4460a0a5fbe0647f5e8c5dc
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-29 14:56:36 -0500

    [update] add lib

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+fn serve_order() { }
+
 mod front_of_house {
    pub mod hosting {
         pub fn add_to_waitlist() { }
@@ -18,4 +20,27 @@ pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String
+    }
+
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            
+        }
+    }
+
+
+    pub fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() { }
+    
 }
