baae4a8448df0a7d7f6bf5201681b5800bd72274
Esteban Victorio <estebanvictorio92@gmail.com>
2019-06-03 21:15:43 -0500

    [update] remove dir

--- collections/target/debug/collections.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Esteban\Desktop\Esteban\Projects\rust-lessons\collections\target\debug\collections.pdb: C:\Users\Esteban\Desktop\Esteban\Projects\rust-lessons\collections\src\main.rs
--- collections/target/debug/deps/collections-b0eda88029e66c7a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Esteban\Desktop\Esteban\Projects\rust-lessons\collections\target\debug\deps\collections-b0eda88029e66c7a.exe: src\main.rs
-
-C:\Users\Esteban\Desktop\Esteban\Projects\rust-lessons\collections\target\debug\deps\collections-b0eda88029e66c7a.d: src\main.rs
-
-src\main.rs:
